5ec4af659f01f1dec9572f4f281ccf00c7d22e56
Breno Soriani <breno.soriani@gmail.com>
2025-07-10 12:17:41 -0300

    JWT implements

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import "express-async-error";
+import { errorHandling } from "./middlewares/userHandling";
 
 import { routes } from "./routes";
 
@@ -7,3 +8,4 @@ export const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(errorHandling);
--- src/routes/tasks-routes.ts
+++ src/routes/tasks-routes.ts
@@ -1,10 +1,27 @@
 import { Router } from "express";
 import { TasksController } from "@/controllers/TaksController";
+import { ensureAuthenticated } from "@/middlewares/ensureAuthenticated";
+import { verifyUser } from "@/middlewares/verifyUser";
 
 export const taskRouter = Router();
 
 const taskController = new TasksController();
 
-taskRouter.post("/create", taskController.create);
-taskRouter.get("/index", taskController.index);
-taskRouter.get("/show/:id", taskController.show);
+taskRouter.post(
+  "/create",
+  ensureAuthenticated,
+  verifyUser(["admin"]),
+  taskController.create
+);
+taskRouter.get(
+  "/index",
+  ensureAuthenticated,
+  verifyUser(["admin"]),
+  taskController.index
+);
+taskRouter.get(
+  "/show/:id",
+  ensureAuthenticated,
+  verifyUser(["admin"]),
+  taskController.show
+);
