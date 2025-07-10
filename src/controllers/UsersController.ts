3eb22389545cf6630343899b145b1758d6ae6b91
Breno Soriani <breno.soriani@gmail.com>
2025-07-10 09:30:11 -0300

    método Create feito

---
+++ src/controllers/UsersController.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from "express";
+import { prisma } from "@/databases/prisma";
+
+import { hash } from "bcrypt";
+import { z } from "zod";
+export class UserController {
+  create = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const bodySchema = z.object({
+        name: z.string().min(3).max(50),
+        email: z.string().email(),
+        password: z.string().min(8).max(50),
+        role: z.enum(["admin", "member"]),
+      });
+
+      const { name, email, password, role } = bodySchema.parse(req.body);
+
+      const hashedPassword = await hash(password, 8);
+
+      const user = prisma.user.create({
+        data: {
+          name,
+          email,
+          password: hashedPassword,
+        },
+      });
+
+      res.json({ user });
+    } catch (error) {
+      next(error);
+    }
+  };
+}
